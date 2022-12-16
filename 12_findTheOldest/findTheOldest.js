1a8fa289c819ca895de4b13b0fc3f89ea9001605
Shahbaaz Siddiqui <shahbaazsidd@gmail.com>
2022-12-16 00:16:40 -0500

    Finish exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((oldest, newPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const newAge = getAge(newPerson.yearOfBirth, newPerson.yearOfDeath);
 
+        return oldestAge > newAge ? oldest : newPerson;
+    })
+
+    // const oldest = people.sort((a, b) => {
+    //     let aAge = 0;
+    //     let bAge = 0;
+    //     if (!a.yearOfDeath) {
+    //         aAge = new Date().getFullYear() - a.yearOfBirth;
+    //     } else {
+    //         aAge = a.yearOfDeath - a.yearOfBirth;
+    //     }
+
+    //     if (!b.yearOfDeath) {
+    //         bAge = new Date().getFullYear() - b.yearOfBirth;
+    //     } else {
+    //         bAge = b.yearOfDeath - b.yearOfBirth;
+    //     }
+
+    //     return aAge > bAge ? -1 : 1;
+    // })
+    // return oldest[0];
 };
+
+const getAge = function (birthYear, deathYear = new Date().getFullYear()) {
+    return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
